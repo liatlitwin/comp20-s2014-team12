1b6c05a5b086c09d6e60f60a5470fd082318945d
Bobby McShane <mcshane.bobby@gmail.com>
2014-04-29 20:38:31 -0400

    testing data rendering

--- client/js/services.js
+++ client/js/services.js
@@ -55,7 +55,7 @@ angular.module('angular-client-side-auth')
         },
         getData: function(success, error) {
             $http.get('/data').success(function(response){
-                success(response);
+                success(JSON.parse(response));
             }).error(error);
         },
         accessLevels: accessLevels,
