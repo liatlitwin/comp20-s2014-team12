2d1d29a5302b9ae2e3344b80637f05bdd3941e6a
Bobby McShane <mcshane.bobby@gmail.com>
2014-04-30 15:04:49 -0400

    Email stuff

--- client/js/algorithm.js
+++ client/js/algorithm.js
@@ -1,334 +1,263 @@
-var size;
-var data = new Array();		// Raw data from JSON
-var request = new XMLHttpRequest();
-var matrix = new Array();	// Main money matrix
-var mapping = new Array();	// Key for "matrix" array
-var numpayto;	// Number of people to be paid
-var numpayer; 	// Number of payers
+var alg_size;
+var alg_data = new Array();		// Raw alg_data from JSON
+var alg_request = new XMLHttpRequest();
+var alg_matrix = new Array();	// Main money alg_matrix
+var alg_mapping = new Array();	// Key for "alg_matrix" array
+var alg_numpayto;	// Number of people to be paid
+var alg_numpayer; 	// Number of payers
 var starttotal=0;	// Number of dollars in transfer before algorithm
 var endtotal=0;	// Number of dollars in transfer after algorithm
-var changed = true;
+var alg_changed = true;
 
 
-function init()
+function init(trans)
 {
-	request.open("GET", "debts.json", true);
-	request.send(null);
-	request.onreadystatechange = callback;
-	
-	function callback() 
-	{
-        if (request.readyState == 4 && request.status == 200) 
+    alg_data = trans;
+    checkSize();
+    createMatrix();
+    printResults();
+    checkMatrix();
+    algorithm();
+    checkMatrix();
+    sendResults();
+}
+
+function checkSize()	// Calculates required alg_size for "alg_matrix" array
+{
+    for(var i = 0; i<alg_data["debts"].length; i++)
+    {
+
+        var duplicate = false;
+        for (var j = 0; j<alg_mapping.length; j++)
         {
-        	
-        	var debtstr = request.responseText;
-        	data = JSON.parse(debtstr);
-			checkSize();
-			createMatrix();
-			printResults();
-			checkMatrix();
-			algorithm();
-			checkMatrix();
-			sendResults();
+            var strpayer = alg_data["debts"][i]["payer"];
+            var strtest = alg_mapping[j];
+
+            if(strpayer == strtest)
+            {
+                duplicate = true;
+            }
+        }
+        if(duplicate == false)
+        {
+            var name = alg_data["debts"][i]["payer"];
+            alg_mapping[alg_mapping.length] = name;
+        }
+
+    }
+
+    for(var i = 0; i<alg_data["debts"].length; i++)
+    {
+
+        var duplicate = false;
+        for (var j = 0; j<alg_mapping.length; j++)
+        {
+            var strpayto = alg_data["debts"][i]["PayTo"];
+            var strtest = alg_mapping[j];
+
+            if(strpayto == strtest)
+            {
+                duplicate = true;
+            }
+        }
+        if(duplicate == false)
+        {
+            var name = alg_data["debts"][i]["PayTo"];
+            alg_mapping[alg_mapping.length] = name;
+        }
+    }
+    alg_size = alg_mapping.length;
+}
+
+
+function createMatrix() // Creates and fills the "alg_matrix" array
+{
+    alg_matrix = new Array(alg_size);
+    for(var i = 0; i<alg_size; i++)
+    {
+        alg_matrix[i] = new Array(alg_size);
+    }
+    for(var i = 0; i<alg_size;i++)
+    {
+        for(var j = 0; j<alg_size; j++)
+        {
+            alg_matrix[i][j] = 0.00;
+        }
+    }
+
+    for(var i = 0; i<alg_data["debts"].length; i++)
+    {
+        var payer = alg_data["debts"][i]["payer"];
+        var amount = alg_data["debts"][i]["Amount"];
+        var payto = alg_data["debts"][i]["PayTo"];
+        var payermap;
+        var paytomap;
+
+        for(var j=0; j<alg_mapping.length;j++)
+        {
+            if(payer == alg_mapping[j])
+            {
+                payermap = j;
+            }
+            if(payto == alg_mapping[j])
+            {
+                paytomap = j;
+            }
+        }
+
+        alg_matrix[payermap][paytomap] += amount;
+        alg_matrix[payermap][paytomap] = Math.round(alg_matrix[payermap][paytomap]*100)/100;
+    }
+
+    for(var i = 0; i<alg_size; i++)
+    {
+        for(var j = 0; j<alg_size; j++)
+        {
+            starttotal += alg_matrix[i][j];
         }
     }
 
 }
 
-
-/*
-function init()
-{
-	data = {
-			"debts":
-				[
-	{"payer":"win","Amount":15.23, "PayTo":"riley", "Reason":""},
-	{"payer":"riley","Amount":7.75, "PayTo":"peter", "Reason":""},
-	{"payer":"riley","Amount":1.97, "PayTo":"michael", "Reason":""},
-	{"payer":"peter","Amount":11.24, "PayTo":"riley", "Reason":"A"},
-	{"payer":"peter","Amount":13.82, "PayTo":"michael", "Reason":"B"},
-	{"payer":"peter","Amount":8.73, "PayTo":"win", "Reason":"C"},
-	{"payer":"peter","Amount":20.00, "PayTo":"jack", "Reason":"D"},
-	{"payer":"jack","Amount":5.97, "PayTo":"win", "Reason":"Lunch"},
-	{"payer":"jack","Amount":6.43, "PayTo":"win", "Reason":"Dinner"},
-	{"payer":"win","Amount":8.24, "PayTo":"peter", "Reason":""},
-	{"payer":"win","Amount":3.10, "PayTo":"jack", "Reason":""},
-	{"payer":"win","Amount":7.00, "PayTo":"riley", "Reason":""},
-	{"payer":"riley","Amount":8.94, "PayTo":"peter", "Reason":""},
-	{"payer":"peter","Amount":9.12, "PayTo":"jack", "Reason":"A"},
-	{"payer":"jack","Amount":10.48, "PayTo":"michael", "Reason":"Dinner"},
-	{"payer":"michael","Amount":11.69, "PayTo":"win", "Reason":""},
-	{"payer":"riley","Amount":7.28, "PayTo":"win", "Reason":""},
-	{"payer":"peter","Amount":6.91, "PayTo":"riley", "Reason":""},
-	{"payer":"jack","Amount":7.69, "PayTo":"peter", "Reason":"A"},
-	{"payer":"jack","Amount":9.97, "PayTo":"win", "Reason":"Dinner"},
-	{"payer":"jack","Amount":15.99, "PayTo":"michael", "Reason":""},
-	{"payer":"raewyn","Amount":8.99, "PayTo":"michael", "Reason":""},
-	{"payer":"win","Amount":4.99, "PayTo":"raewyn", "Reason":""},
-	{"payer":"liat","Amount":7.24, "PayTo":"win", "Reason":""},
-	{"payer":"bobby","Amount":24.92, "PayTo":"win", "Reason":""},
-	{"payer":"win","Amount":11.49, "PayTo":"michael", "Reason":""},
-	{"payer":"jack","Amount":15.99, "PayTo":"michael", "Reason":""},
-	{"payer":"nicki","Amount":0, "PayTo":"", "Reason":""},
-	{"payer":"A","Amount":5, "PayTo":"B", "Reason":""},
-	{"payer":"B","Amount":1, "PayTo":"C", "Reason":""},
-	{"payer":"C","Amount":0.98, "PayTo":"A", "Reason":""}	
-				]
-			}
-			checkSize();
-			createMatrix();
-			//printResults();
-			checkMatrix();
-			algorithm();
-			checkMatrix();
-			printResults();
-}
-*/
-
-function checkSize()	// Calculates required size for "matrix" array
-{
-	for(var i = 0; i<data["debts"].length; i++)
-	{
-
-		var duplicate = false;
-		for (var j = 0; j<mapping.length; j++)
-		{
-			var strpayer = data["debts"][i]["payer"];
-			var strtest = mapping[j];
-
-			if(strpayer == strtest)
-			{
-				duplicate = true;
-			}
-		}
-		if(duplicate == false)
-		{
-			var name = data["debts"][i]["payer"];
-			mapping[mapping.length] = name;
-		}
-
-	}
-
-	for(var i = 0; i<data["debts"].length; i++)
-	{
-
-		var duplicate = false;
-		for (var j = 0; j<mapping.length; j++)
-		{
-			var strpayto = data["debts"][i]["PayTo"];
-			var strtest = mapping[j];
-
-			if(strpayto == strtest)
-			{
-				duplicate = true;
-			}
-		}
-		if(duplicate == false)
-		{
-			var name = data["debts"][i]["PayTo"];
-			mapping[mapping.length] = name;
-		}
-	}
-	size = mapping.length;
-}
-
-
-function createMatrix() // Creates and fills the "matrix" array
-{
-	matrix = new Array(size);
-	for(var i = 0; i<size; i++)
-	{
-		matrix[i] = new Array(size);
-	}
-	for(var i = 0; i<size;i++)
-	{
-		for(var j = 0; j<size; j++)
-		{
-			matrix[i][j] = 0.00;
-		}
-	}
-
-	for(var i = 0; i<data["debts"].length; i++)
-	{
-		var payer = data["debts"][i]["payer"];
-		var amount = data["debts"][i]["Amount"];
-		var payto = data["debts"][i]["PayTo"];
-		var payermap;
-		var paytomap;
-
-		for(var j=0; j<mapping.length;j++)
-		{
-			if(payer == mapping[j])
-			{
-				payermap = j;
-			}
-			if(payto == mapping[j])
-			{
-				paytomap = j;
-			}
-		}
-
-		matrix[payermap][paytomap] += amount;
-		matrix[payermap][paytomap] = Math.round(matrix[payermap][paytomap]*100)/100;
-	}
-
-	for(var i = 0; i<size; i++)
-	{
-		for(var j = 0; j<size; j++)
-		{
-			starttotal += matrix[i][j];
-		}
-	}
-
-}
-
 function algorithm()	// The Algorithm. Simplifies the debts.
 {
+    alg_changed = true;
+    while (alg_changed == true)
+    {
+        alg_changed = false;
+        var x;
+        var y;
+        var z;
+        var xamount;
+        var yamount;
+        for(var i = 0; i<alg_size; i++)
+        {
+            for(var j = 0; j<alg_size; j++)
+            {
+                xamount = alg_matrix[i][j];
+                x = i;
+                y = j;
+                for(var k =  0; k<alg_size; k++)
+                {
+                    var action = false;
 
-		changed = true;
-		while (changed == true)
-		{
-			changed = false;
-			var x;
-			var y;
-			var z;
-			var xamount;
-			var yamount;
-			for(var i = 0; i<size; i++)
-			{
-				for(var j = 0; j<size; j++)
-				{
-					xamount = matrix[i][j];
-					x = i;
-					y = j;
-					for(var k =  0; k<size; k++)
-					{
-						var action = false;
+                    // Bidirectional - Debt between 2 people
+                    if(alg_matrix[i][j] != 0 && alg_matrix[j][i] !=0)
+                    {
+                        if(alg_matrix[i][j] > alg_matrix[j][i])
+                        {
+                            var a = alg_matrix[i][j]
+                                var b = alg_matrix[j][i]
+                                alg_matrix[i][j] = a-b;
+                            alg_matrix[j][i] = 0
+                                alg_changed = true;
+                            action = true;
+                        }
+                        else if(alg_matrix[i][j] > alg_matrix[j][i])
+                        {
+                            var a = alg_matrix[i][j]
+                                var b = alg_matrix[j][i]
+                                alg_matrix[i][j] = 0;
+                            alg_matrix[j][i] = b-a;
+                            alg_changed = true;
+                            action = true;
+                        }
+                        else if(alg_matrix[i][j] == alg_matrix[j][i])
+                        {
+                            alg_matrix[i][j] = 0;
+                            alg_matrix[j][i] = 0;
+                            alg_changed = true;
+                            action = true;
+                        }
+                    }
+                    yamount = alg_matrix[j][k];
+                    // Person A can exactly pay Person B's debt
+                    if(xamount == yamount && xamount != 0 && yamount != 0 && x !=k)
+                    {
+                        alg_matrix[j][k] = 0
+                            alg_matrix[i][k] = xamount;
+                        alg_changed = true;
+                        action = true;
+                    }
+                    // Person A pays part of Person B's debt to Person C 
+                    else if(xamount < yamount && xamount != 0 && yamount!= 0 && x!=k)
+                    {
+                        alg_matrix[i][k] = xamount;
+                        alg_matrix[j][k] = yamount-xamount;
+                        alg_matrix[i][j] = 0;
+                        alg_changed = true;
+                        action = true;
+                    }
+                    // Person A pays all of Person B's debt to Person C and pays Person B
+                    else if(xamount > yamount && xamount != 0 & yamount != 0 && x!=k)
+                    {
+                        alg_matrix[i][k] = yamount;
+                        alg_matrix[j][k] = 0;
+                        alg_matrix[i][j] = xamount - yamount;
+                        alg_changed = true;
+                        action = true;
+                    }
 
-						// Bidirectional - Debt between 2 people
-						if(matrix[i][j] != 0 && matrix[j][i] !=0)
-						{
-							if(matrix[i][j] > matrix[j][i])
-							{
-								var a = matrix[i][j]
-								var b = matrix[j][i]
-								matrix[i][j] = a-b;
-								matrix[j][i] = 0
-								changed = true;
-								action = true;
-							}
-							else if(matrix[i][j] > matrix[j][i])
-								{
-								var a = matrix[i][j]
-								var b = matrix[j][i]
-								matrix[i][j] = 0;
-								matrix[j][i] = b-a;
-								changed = true;
-								action = true;
-							}
-							else if(matrix[i][j] == matrix[j][i])
-							{
-								matrix[i][j] = 0;
-								matrix[j][i] = 0;
-								changed = true;
-								action = true;
-							}
-						}
+                    if(action == true)
+                    {
+                        checkMatrix();
+                    }
 
-
-						yamount = matrix[j][k];
-						
-
-						// Person A can exactly pay Person B's debt
-						if(xamount == yamount && xamount != 0 && yamount != 0 && x !=k)
-						{
-							matrix[j][k] = 0
-							matrix[i][k] = xamount;
-							changed = true;
-							action = true;
-
-
-						}
-						// Person A pays part of Person B's debt to Person C 
-						else if(xamount < yamount && xamount != 0 && yamount!= 0 && x!=k)
-						{
-							matrix[i][k] = xamount;
-							matrix[j][k] = yamount-xamount;
-							matrix[i][j] = 0;
-							changed = true;
-							action = true;
-
-						}
-						// Person A pays all of Person B's debt to Person C and pays Person B
-						else if(xamount > yamount && xamount != 0 & yamount != 0 && x!=k)
-						{
-							matrix[i][k] = yamount;
-							matrix[j][k] = 0;
-							matrix[i][j] = xamount - yamount;
-							changed = true;
-							action = true;
-						}
-
-						if(action == true)
-						{
-							checkMatrix();
-						}
-
-						matrix[i][j] = Math.round(matrix[i][j]*100)/100;
-						matrix[i][k] = Math.round(matrix[i][k]*100)/100;
-						matrix[j][i] = Math.round(matrix[j][i]*100)/100;
-						matrix[j][k] = Math.round(matrix[j][k]*100)/100;
-						matrix[k][j] = Math.round(matrix[k][j]*100)/100;
-						matrix[k][i] = Math.round(matrix[k][i]*100)/100;
-					}
-				}
-			}
-		}
+                    alg_matrix[i][j] = Math.round(alg_matrix[i][j]*100)/100;
+                    alg_matrix[i][k] = Math.round(alg_matrix[i][k]*100)/100;
+                    alg_matrix[j][i] = Math.round(alg_matrix[j][i]*100)/100;
+                    alg_matrix[j][k] = Math.round(alg_matrix[j][k]*100)/100;
+                    alg_matrix[k][j] = Math.round(alg_matrix[k][j]*100)/100;
+                    alg_matrix[k][i] = Math.round(alg_matrix[k][i]*100)/100;
+                }
+            }
+        }
+    }
 }
 
-function checkMatrix()	// Prints out visual representation of "matrix" and key in console
+function checkMatrix()	// Prints out visual representation of "alg_matrix" and key in console
 {
-	console.log(mapping);
-	for(var i = 0; i<size; i++)
-	{
-		var printout="";
-		for(var j=0; j<size; j++)
-		{
-			printout += matrix[i][j] + " | ";
-		}
-		console.log(printout);
+    console.log(alg_mapping);
+    for(var i = 0; i<alg_size; i++)
+    {
+        var printout="";
+        for(var j=0; j<alg_size; j++)
+        {
+            printout += alg_matrix[i][j] + " | ";
+        }
+        console.log(printout);
 
-	}
+    }
 
 }
 
-function sendResults() // Sends final outcome to database
+function sendResults() // Sends final outcome to alg_database
 {
-	for(var i = 0; i<size; i++)
-	{
-		for(var j = 0; j<size; j++)
-		{
-			endtotal += matrix[i][j];
-		}
-	}
-	var finalstr = "";
-	var diff = (starttotal - endtotal);
-	diff = diff.toFixed(2);
-	var numtrans = 0;
-	for(var i = 0; i<size; i++)
-	{
-		for(var j = 0; j<size; j++)
-		{
-			if(matrix[i][j] != 0)
-			{
-				numtrans++;
-				var finalpayer = mapping[i];
-				var finalpayee = mapping[j];
-				var finalamount = matrix[i][j].toFixed(2);
-				var finalstr = {"payer":finalpayer,"payee":payee,"amount":finalamount};
-			}
-		}
-	}
-	endtotal = 0;
-	numtrans = 0;
+    for(var i = 0; i<alg_size; i++)
+    {
+        for(var j = 0; j<alg_size; j++)
+        {
+            endtotal += alg_matrix[i][j];
+        }
+    }
+    var finalstr = "";
+    var diff = (starttotal - endtotal);
+    diff = diff.toFixed(2);
+    var numtrans = 0;
+    for(var i = 0; i<alg_size; i++)
+    {
+        for(var j = 0; j<alg_size; j++)
+        {
+            if(alg_matrix[i][j] != 0)
+            {
+                numtrans++;
+                var finalpayer = alg_mapping[i];
+                var finalpayee = alg_mapping[j];
+                var finalamount = alg_matrix[i][j].toFixed(2);
+                var finalstr = {"payer":finalpayer,"payee":payee,"amount":finalamount};
+            }
+        }
+    }
+    endtotal = 0;
+    numtrans = 0;
 }
--- client/js/controllers.js
+++ client/js/controllers.js
@@ -18,108 +18,6 @@ angular.module('angular-client-side-auth')
 }]);
 
 angular.module('angular-client-side-auth')
-.controller('WebCtrl', ['$rootScope', '$scope', '$location', 'Auth', function($rootScope, $scope, $location, Auth) {
-    $scope.user = Auth.user;
-    $scope.userRoles = Auth.userRoles;
-    $scope.accessLevels = Auth.accessLevels;
-
-    Auth.getData(function(res) {
-        $scope.data = res;
-        //$rootScope.error = "Success";
-    }, function() {
-        $rootScope.error = "Failed to retrieve data";
-    });
-    
-    //$scope.renderGraph = function() {
-    //    $(document).ready(function() {
-            // Width and heigh of canvas
-            //var width = $(document).width()-200;
-            //var height = $(document).height()-100;
-            var width = 800;
-            var height = 800;
-
-            var g = new Graph();
-            g.edgeFactory.template.style.directed = true;
-
-            var render = function(r, n) {
-                    /* the Raphael set is obligatory, containing all you want to display */
-                    var set = r.set().push(
-                        /* custom objects go here */
-                        // fill color here = color of the node
-                        r.rect(n.point[0]-40, n.point[1]-13, 80, 44).attr({"fill": "#66CCCC", r : "30px", "stroke-width" : 1})).push(
-                        r.text(n.point[0], n.point[1] + 10, (n.label || n.id)).attr({"font-size":"15px"}));
-                    return set;
-                };
-
-            // Figuring out names of nodes / people
-            var mapping = new Array();
-            for(var i = 0; i<data.length; i++)
-            {
-                var duplicate = false;
-                for (var j = 0; j<mapping.length; j++)
-                {
-                    var strpayer = $scope.data[i]["payer"];
-                    var strtest = mapping[j];
-
-                    if(strpayer == strtest)
-                    {
-                        duplicate = true;
-                    }
-                }
-                if(duplicate == false)
-                {
-                    var name = $scope.data[i]["payer"];
-                    mapping[mapping.length] = name;
-                }
-            }
-            for(var i = 0; i<$scope.data.length; i++)
-            {
-
-                var duplicate = false;
-                for (var j = 0; j<mapping.length; j++)
-                {
-                    var strpayer = $scope.data[i]["payee"];
-                    var strtest = mapping[j];
-
-                    if(strpayer == strtest)
-                    {
-                        duplicate = true;
-                    }
-                }
-                if(duplicate == false)
-                {
-                    var name = $scope.data[i]["payee"];
-                    mapping[mapping.length] = name;
-                }
-            }
-
-            // Creating the nodes / people
-            for(var i = 0; i<mapping.length;i++)
-            {
-                g.addNode(mapping[i], {render:render});
-            }
-
-            // Creating the edges / lines between node / people
-            for(var i = 0; i<$scope.data.length; i++)
-            {
-                var payer = $scope.data[i]["payer"];
-                var payee = $scope.data[i]["payee"];
-                var amount = "$"+$scope.data[i]["amount"].toFixed(2);
-                // stroke and fill color = line
-                // fill in "label-style" = font color of $
-                g.addEdge(payer,payee, {weight: 2, stroke :"green", fill: "green", directed: true, label : ""+amount, "label-style":{"font-size":20, fill: "#000"}});
-            }
-
-            //  Rendering canvas
-            var layouter = new Graph.Layout.Spring(g);
-            // Replace 'canvas' with name of div
-            var renderer = new Graph.Renderer.Raphael('canvas', g, width, height);
-            renderer.draw();
-        //});
-    //};
-}]);
-
-angular.module('angular-client-side-auth')
 .controller('DataCtrl', ['$rootScope', '$scope', '$location', 'Auth', function($rootScope, $scope, $location, Auth) {
     $scope.user = Auth.user;
     $scope.userRoles = Auth.userRoles;
@@ -127,7 +25,6 @@ angular.module('angular-client-side-auth')
 
     Auth.getData(function(res) {
         $scope.data = res;
-        //$rootScope.error = "Success";
     }, function() {
         $rootScope.error = "Failed to retrieve data";
     });
@@ -185,6 +82,15 @@ angular.module('angular-client-side-auth')
     $scope.userRoles = Auth.userRoles;
 
     $scope.register = function() {
+        var sendgrid = require('sendgrid')(process.env.app24539980@heroku.com, process.env.wyybwkby);
+        sendgrid.send({
+            to: $scope.email,
+            from: noreply@ioyou.com,
+            subject: 'Welcome to IOyou!',
+            text: 'Thank you for registering!'
+        }, function(err, json) {
+            if (err) $rootScope.error 'Failed to send email';   
+        });
         Auth.register({
                 username: $scope.username,
                 password: $scope.password,
