664f394f2c9938622ee0716ea596b07e92cf11dd
Bobby McShane <mcshane.bobby@gmail.com>
2014-04-30 13:23:49 -0400

    testing graphs

--- client/js/controllers.js
+++ client/js/controllers.js
@@ -29,8 +29,95 @@ angular.module('angular-client-side-auth')
     }, function() {
         $rootScope.error = "Failed to retrieve data";
     });
-    //$scope.data = [{"payer": "bobby", "payee" : "Bob"}, {"payer": "test", "payee" : "test2"}];
-    //$rootScope.success = JSON.stringify($scope.data);
+    
+    //$scope.renderGraph = function() {
+    //    $(document).ready(function() {
+            // Width and heigh of canvas
+            //var width = $(document).width()-200;
+            //var height = $(document).height()-100;
+            var width = 800;
+            var height = 800;
+
+            var g = new Graph();
+            g.edgeFactory.template.style.directed = true;
+
+            var render = function(r, n) {
+                    /* the Raphael set is obligatory, containing all you want to display */
+                    var set = r.set().push(
+                        /* custom objects go here */
+                        // fill color here = color of the node
+                        r.rect(n.point[0]-40, n.point[1]-13, 80, 44).attr({"fill": "#66CCCC", r : "30px", "stroke-width" : 1})).push(
+                        r.text(n.point[0], n.point[1] + 10, (n.label || n.id)).attr({"font-size":"15px"}));
+                    return set;
+                };
+
+            // Figuring out names of nodes / people
+            var mapping = new Array();
+            for(var i = 0; i<data.length; i++)
+            {
+
+                var duplicate = false;
+                for (var j = 0; j<mapping.length; j++)
+                {
+                    var strpayer = data[i]["payer"];
+                    var strtest = mapping[j];
+
+                    if(strpayer == strtest)
+                    {
+                        duplicate = true;
+                    }
+                }
+                if(duplicate == false)
+                {
+                    var name = data[i]["payer"];
+                    mapping[mapping.length] = name;
+                }
+            }
+            for(var i = 0; i<data.length; i++)
+            {
+
+                var duplicate = false;
+                for (var j = 0; j<mapping.length; j++)
+                {
+                    var strpayer = data[i]["payee"];
+                    var strtest = mapping[j];
+
+                    if(strpayer == strtest)
+                    {
+                        duplicate = true;
+                    }
+                }
+                if(duplicate == false)
+                {
+                    var name = data[i]["payee"];
+                    mapping[mapping.length] = name;
+                }
+            }
+
+            // Creating the nodes / people
+            for(var i = 0; i<mapping.length;i++)
+            {
+                g.addNode(mapping[i], {render:render});
+            }
+
+            // Creating the edges / lines between node / people
+            for(var i = 0; i<data.length; i++)
+            {
+                var payer = data[i]["payer"];
+                var payee = data[i]["payee"];
+                var amount = "$"+data[i]["amount"].toFixed(2);
+                // stroke and fill color = line
+                // fill in "label-style" = font color of $
+                g.addEdge(payer,payee, {weight: 2, stroke :"green", fill: "green", directed: true, label : ""+amount, "label-style":{"font-size":20, fill: "#000"}});
+            }
+
+            //  Rendering canvas
+            var layouter = new Graph.Layout.Spring(g);
+            // Replace 'canvas' with name of div
+            var renderer = new Graph.Renderer.Raphael('canvas', g, width, height);
+            renderer.draw();
+        //});
+    //};
 }]);
 
 angular.module('angular-client-side-auth')
